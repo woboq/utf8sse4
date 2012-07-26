eb56ae7054b2bc1a6e4181fac2e1ccf8fd1f0631
Olivier Goffart <ogoffart@woboq.com>
2012-07-26 17:25:39 +0200

    first commit

---
+++ benchmark.cc
@@ -0,0 +1,58 @@
+#include <QtCore/QtCore>
+#include <iostream>
+
+#ifdef USE_ICONV
+#include <iconv.h>
+#endif
+
+void fromUtf8_sse(const char *&src, int &len, ushort * &dst);
+
+int main(int argc, char **argv) {
+    QCoreApplication app(argc, argv);
+    QString filename = app.arguments().value(1);
+    if (filename.isEmpty()) {
+        std::cout << "Usage: " << argv[0] << " <utf-8 file>. " << std::endl;
+        exit(-1);
+    }
+
+    QFile file(filename);
+    if (!file.open(QFile::ReadOnly)) {
+        printf("Unable to open %s for reading\n", qPrintable(filename));
+        exit(-1);
+    }
+
+    QByteArray source = file.readAll();
+
+    QString str;
+    str.resize(source.size()*3);
+
+#ifdef USE_ICONV
+    iconv_t iconv_cd = iconv_open("UTF-8", "UTF-16");
+#endif
+
+    const int Iterations = 10000;
+    QElapsedTimer timer;
+    timer.start();
+    for (int i = 0; i < Iterations; ++i) {
+        size_t len = source.size();
+        char *dst = reinterpret_cast<char *>(str.begin());
+        size_t outlen = str.length()*2;
+#ifdef USE_ICONV
+        char *src = source.begin();
+        iconv(iconv_cd, &src, &len, &dst, &outlen);
+#elif defined USE_U8U16
+        extern size_t u8u16(char**inbuf,size_t*inbytesleft,char**outbuf,size_t*outbytesleft);
+        char *src = source.begin();
+        u8u16(&src, &len, &dst, &outlen);
+#else
+        const char *src = source.constBegin();
+        fromUtf8(&src, &len, &dst, &outlen);
+#endif
+    }
+
+    std::cout << "Done in " <<  (timer.nsecsElapsed() / Iterations) << "ns"<< std::endl;
+
+#ifdef USE_ICONV
+    iconv_close(iconv_cd);
+#endif
+}
